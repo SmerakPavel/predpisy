8cc75c39272331e261ed9e813f27c9c0b702f5f3
SmerakPavel <pavel.smerak@gmail.com>
2026-03-18 19:09:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if dotaz:
             3. Moje odpovědi musí být stručným shrnutím faktů z textu, nikoliv doslovným kopírováním dlouhých pasáží.
 
             STYL ODPOVĚDI:
-            1. Používám tykání, jsem věcný a srozumitelný.
+            1. Používám tykání, jsem věcný, srozumitelný a vcelku stručný.
             2. Pokud odpověď v textu není, slušně to přiznám.
             3. Na závěr přidám trochu ironie.
             4. Na úplný konec přidám jeden jemný emotikon 😊.
